1cd9532ca40a94039678a4c10eed7167853d2d93
Luke Lee <samsung3star@gmail.com>
2015-03-19 13:18:08 -0400

    Fibonacci completed

--- HW_20150320/src/nyc/c4q/lukesterlee/Fibonacci.java
+++ HW_20150320/src/nyc/c4q/lukesterlee/Fibonacci.java
@@ -1,27 +1,29 @@
 package nyc.c4q.lukesterlee;
 
 /**
+ * Access Code 2.1
  * Created by Luke Lee on 3/17/15.
+ * The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+ * Write a program which accepts as input an integer N and returns the N-th Fibonacci number.
+ * To test whether your program returned the correct number, go to Wolfram Alpha,
+ * and enter fibonacci(N) where N is your input integer
  */
 public class Fibonacci {
 
-    public static void print(String text) {
-        System.out.print(text);
-    }
+    public static int fibonacci(int nthNum) {
 
-    public static int fibonacci(int size) {
-
-        if (size == 1)
+        // If N-th number is 1, return 0.
+        if (nthNum == 1)
             return 0;
-        else if (size == 2)
+        // If N-th number is 2, return 1.
+        else if (nthNum == 2)
             return 1;
+        // Otherwise, return the value of N-th-1 number and N-th-2 number.
         else
-            return fibonacci(size-2) + fibonacci(size-1);
+            return fibonacci(nthNum-2) + fibonacci(nthNum-1);
     }
 
     public static void main(String[] args) {
-
-        print("" + fibonacci(20));
-
+        System.out.println(fibonacci(20));
     }
 }
