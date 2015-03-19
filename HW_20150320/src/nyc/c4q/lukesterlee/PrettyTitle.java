96a0e4f34c46c541bda31051a79a81047720f0c6
Luke Lee <samsung3star@gmail.com>
2015-03-19 12:54:59 -0400

    PrettyTitle completed (comments, merging into one method)

--- HW_20150320/src/nyc/c4q/lukesterlee/PrettyTitle.java
+++ HW_20150320/src/nyc/c4q/lukesterlee/PrettyTitle.java
@@ -1,7 +1,11 @@
 package nyc.c4q.lukesterlee;
 
 /**
+ * Access Code 2.1
  * Created by Luke Lee on 3/17/15.
+ * A Pretty Title exercise has a method called printTitle that prints a phrase as a title by,
+ *    1. converting it to title capitalization
+ *    2. underlining each word, i.e. underlying all characters except spaces
  */
 
 import java.util.Scanner;
@@ -14,50 +18,41 @@ public class PrettyTitle {
 
     public static void printTitle(String text, char symbol) {
 
-        print(text+ "\n");
-
-        for (int i=0; i<text.length(); i++) {
-            if (text.charAt(i) != ' ')
-                print("*");
-            else
-                print(" ");
-        }
-
-    }
-
-    public static String capitalize(String text) {
-
         String[] preposition = new String[]{"and", "to", "for", "at" ,"by", "of", "with", "on", "in", "from",
                 "as", "but"};
         Scanner input = new Scanner(text);
         boolean isPreposition = false;
         String word;
         String capitalizedString = "";
+
         while (input.hasNext()) {
             word = input.next();
-
+            // check whether the word is preposition or not.
             for (int i=0; i<preposition.length; i++) {
                 if (word.equals(preposition[i])) {
                     isPreposition = true;
                     break;
                 }
             }
-            if (!isPreposition) {
+            // if the word is not in the preposition list, capitalize the first letter.
+            if (!isPreposition)
                 word = word.substring(0, 1).toUpperCase() + word.substring(1);
-            }
             capitalizedString += word + " ";
             isPreposition = false;
         }
 
-        return capitalizedString;
+        print(capitalizedString + "\n");
+
+        // This loop prints '*' under the words; it skips when they encounter white spaces.
+        for (int i=0; i<text.length(); i++) {
+            if (text.charAt(i) != ' ')
+                print(symbol + "");
+            else
+                print(" ");
+        }
     }
 
     public static void main(String[] args) {
-
-        printTitle(capitalize("Hello, this is Luke Lee and William"), '*');
-
-
-
+        printTitle("a tale of two cities", '*');
     }
-
 }
--- VerbosityCalculator/src/nyc/c4q/ac21/VerbosityCalculator.java
+++ VerbosityCalculator/src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -8,24 +8,45 @@ import java.util.Scanner;
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
+
+    public static void print(String text) {
+        System.out.println(text);
+    }
     public static void main(String[] args){
 
+        // to count the number of words.
+        int wordCount = 0;
+        int sentenceCount = 0;
         // An abstract representation of a File
-        File file = new File("PATH to text file");
+        File file = new File("/Users/Willee/Desktop/accesscode/VerbosityCalculator/resources/pg98.txt");
+        File file2 = new File("/Users/Willee/Desktop/accesscode/VerbosityCalculator/resources/pg2701.txt");
 
         try{
             Scanner sc = new Scanner(file);
-            /**
-             *  Your CODE goes here
-             *
-             *
-             *
-             *
-             */
+
+            String word;
+            while (sc.hasNext()) {
+
+                word = sc.next();
+                wordCount++;
+
+                for(int i=0; i<word.length(); i++) {
+                    if ('.' == word.charAt(i))
+                        sentenceCount++;
+                }
+
+            }
+
 
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
+
+        print(wordCount + "");
+        print(sentenceCount + "");
+
+
+
     }
 }
