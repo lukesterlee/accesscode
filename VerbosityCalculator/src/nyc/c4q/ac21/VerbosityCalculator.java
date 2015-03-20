fb36e4b3ef7b4e3d0e8beb50203d3a7dd2bb1211
Luke Desktop <lukesterlee@gmail.com>
2015-03-20 14:13:56 -0400

    Verbosity Calculator completed

--- VerbosityCalculator/src/nyc/c4q/ac21/VerbosityCalculator.java
+++ VerbosityCalculator/src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -6,6 +6,8 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
+ * Access Code 2.1
+ * Created by Luke Lee on 03/20/15
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
@@ -18,19 +20,35 @@ public class VerbosityCalculator {
         DecimalFormat df = new DecimalFormat("#.00");
         int wordCount = 0;
         int sentenceCount = 0;
+
         String word;
         try {
             Scanner reader = new Scanner(file);
-
             while (reader.hasNext()) {
-
                 word = reader.next();
                 wordCount++;
 
-                if ('.' == word.charAt(word.length()-1))
+                // if the word contains "." or "!" or "?", then count it as a sentence.
+                if (word.contains(".") || word.contains("!") || word.contains("?"))
                     sentenceCount++;
-
+                // don't count periods with Mr. and Mrs.
+                if (word.equalsIgnoreCase("mr.") || word.equalsIgnoreCase("mrs."))
+                    sentenceCount--;
             }
+
+            Scanner reader2 = new Scanner(file);
+            while(reader2.hasNextLine()) {
+                word = reader2.nextLine();
+                for (int i=0; i<word.length()-3; i++) {
+                    // if "?" is followed by quotation mark and white space and lower case letter, then don't count it as a sentence.
+                    if (word.charAt(i) == '?' && word.charAt(i+1) == '"' && word.charAt(i+2) == ' ' && word.charAt(i+3) == Character.toLowerCase(word.charAt(i + 3)))
+                        sentenceCount--;
+                    // if "!" is followed by quotation mark and white space and lower case letter, then don't count it as a sentence.
+                    if (word.charAt(i) == '!' && word.charAt(i+1) == '"' && word.charAt(i+2) == ' ' && word.charAt(i+3) == Character.toLowerCase(word.charAt(i+3)))
+                        sentenceCount--;
+                }
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -43,13 +61,26 @@ public class VerbosityCalculator {
     public static void main(String[] args){
 
         // An abstract representation of a File
-        File dickens = new File("/Users/Willee/Desktop/accesscode/VerbosityCalculator/resources/pg98.txt");
-        File mobydick = new File("/Users/Willee/Desktop/accesscode/VerbosityCalculator/resources/pg2701.txt");
+        File dickens = new File("C:\\Users\\Luke\\Desktop\\accesscode\\VerbosityCalculator\\resources\\pg98.txt");
+        File mobydick = new File("C:\\Users\\Luke\\Desktop\\accesscode\\VerbosityCalculator\\resources\\pg2701.txt");
 
+        print("A Tale of Two Cities, by Charles Dickens");
+        /*
+         * OUTPUT of printAverage(dickens);
+         * This book has total 138879 words.
+         * This book has total 7681 sentences.
+         * Total average is 18.08
+         */
         printAverage(dickens);
+
+        print("Moby Dick; or The Whale by Herman Melville");
+        /*
+         * OUTPUT of printAverage(mobydick);
+         * This book has total 215135 words.
+         * This book has total 10256 sentences.
+         * Total average is 20.98
+         */
         printAverage(mobydick);
-
-
 
     }
 }
