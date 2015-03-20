7fc0c326891819511649cf72bd333af63c32e958
Luke Lee <samsung3star@gmail.com>
2015-03-20 11:10:24 -0400

    0319

---
+++ AC_20150319/src/nyc/c4q/lukesterlee/BirthdayCalculator.java
@@ -0,0 +1,77 @@
+package nyc.c4q.lukesterlee;
+
+/**
+ * Access Code 2.1
+ * Created by Luke Lee on 3/19/15.
+ */
+
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class BirthdayCalculator {
+
+    public static void print(String text) {
+        System.out.print(text);
+    }
+
+    public static boolean isLeapYear(int year) {
+
+        if (year%4 != 0)
+            return false;
+        else if (year%100 != 0)
+            return true;
+        else if (year%400 != 0)
+            return false;
+        else
+            return true;
+    }
+
+    public static int calculate(int today, int birthday, int year) {
+
+
+        int daysleft;
+        if (birthday >= today) {
+            daysleft = birthday - today;
+        } else {
+            if (isLeapYear(year+1)) {
+                birthday += 1;
+            }
+            daysleft = (365 - today) + birthday;
+        }
+
+        return daysleft;
+    }
+
+    public static void main(String[] args) {
+
+        int year;
+        int month;
+        int day;
+        int today;
+        int birthday;
+
+        Scanner input = new Scanner(System.in);
+        Calendar rightNow = Calendar.getInstance();
+
+        today = rightNow.get(Calendar.DAY_OF_YEAR);
+        year = rightNow.get(Calendar.YEAR);
+        print(today + "");
+        print("Please enter your birthday:\n");
+        print("What is the month (1-12): ");
+
+        month = input.nextInt();
+
+        print("What is the day (1-31): ");
+
+        day = input.nextInt();
+
+        rightNow.set(year,month-1,day);
+        birthday = rightNow.get(Calendar.DAY_OF_YEAR);
+        print(birthday + "");
+
+        print("There are " + calculate(today,birthday,year) + " days left!");
+
+
+
+    }
+}
---
+++ AC_20150319/src/nyc/c4q/lukesterlee/Person.java
@@ -0,0 +1,40 @@
+package nyc.c4q.lukesterlee;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public static void main(String[] args) {
+
+        Person fred = new Person("Fred", 28);
+        Person bob = fred;
+        bob.setName("John");
+        System.out.println(fred.getName());
+    }
+}
