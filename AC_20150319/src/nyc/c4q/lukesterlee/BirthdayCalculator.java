cabf3f9192f667b37e938d1d1b8f59f86fc85674
Luke Lee <samsung3star@gmail.com>
2015-03-24 20:01:46 -0400

    Greg's awesome method

--- AC_20150319/src/nyc/c4q/lukesterlee/BirthdayCalculator.java
+++ AC_20150319/src/nyc/c4q/lukesterlee/BirthdayCalculator.java
@@ -27,8 +27,6 @@ public class BirthdayCalculator {
     }
 
     public static int calculate(int today, int birthday, int year) {
-
-
         int daysleft;
         if (birthday >= today) {
             daysleft = birthday - today;
@@ -38,17 +36,16 @@ public class BirthdayCalculator {
             }
             daysleft = (365 - today) + birthday;
         }
-
         return daysleft;
     }
 
     public static void main(String[] args) {
 
         int year;
-        int month;
-        int day;
+        int birthMonth;
+        int birthDay;
         int today;
-        int birthday;
+        int birthdayInDays;
 
         Scanner input = new Scanner(System.in);
         Calendar rightNow = Calendar.getInstance();
@@ -59,18 +56,23 @@ public class BirthdayCalculator {
         print("Please enter your birthday:\n");
         print("What is the month (1-12): ");
 
-        month = input.nextInt();
+        birthMonth = input.nextInt();
 
         print("What is the day (1-31): ");
 
-        day = input.nextInt();
+        birthDay = input.nextInt();
 
-        rightNow.set(year,month-1,day);
-        birthday = rightNow.get(Calendar.DAY_OF_YEAR);
-        print(birthday + "");
+        rightNow.set(year,birthMonth-1,birthDay);
+        birthdayInDays = rightNow.get(Calendar.DAY_OF_YEAR);
+        print(birthdayInDays + "");
 
-        print("There are " + calculate(today,birthday,year) + " days left!");
+        print("There are " + calculate(today,birthdayInDays,year) + " days left!");
 
+
+        /*
+        int a =
+        int
+         */
 
 
     }
