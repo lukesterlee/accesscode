18c9ec3468ffa52640c33f8b9e19e193f7d1b077
Luke Desktop <lukesterlee@gmail.com>
2015-03-25 12:53:47 -0400

    HW set up

--- HW_20150327/src/nyc/c4q/lukesterlee/Person.java
+++ HW_20150327/src/nyc/c4q/lukesterlee/Person.java
@@ -3,5 +3,43 @@ package nyc.c4q.lukesterlee;
 /**
  * Created by Luke Lee on 3/25/15.
  */
+/**
+ * A Person class
+ */
 public class Person {
+
+    private String name;
+    private String phoneNumber;
+    private String city;
+
+    public void Person(){
+    }
+
+    public void Person(String name){
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
