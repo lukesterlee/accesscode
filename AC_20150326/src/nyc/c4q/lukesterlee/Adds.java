a470405f2fce746bd506a56605a1a9edf40635a7
Luke Lee <samsung3star@gmail.com>
2015-03-26 19:28:27 -0400

    Initial commit

---
+++ AC_20150326/src/nyc/c4q/lukesterlee/Adds.java
@@ -0,0 +1,25 @@
+package nyc.c4q.lukesterlee;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Adds {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        int num = 0;
+
+        System.out.println("Give me some numbers to add:");
+        while (num != -2) {
+            num = input.nextInt();
+            numbers.add(num);
+        }
+
+        System.out.println("Thanks!");
+
+    }
+}
