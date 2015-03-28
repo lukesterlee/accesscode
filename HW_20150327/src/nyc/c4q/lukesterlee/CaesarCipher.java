a200fe9bd12524e43bd649524e78f900eea293dd
Luke Desktop <lukesterlee@gmail.com>
2015-03-27 20:35:19 -0400

    fixed a bug

--- HW_20150327/src/nyc/c4q/lukesterlee/CaesarCipher.java
+++ HW_20150327/src/nyc/c4q/lukesterlee/CaesarCipher.java
@@ -29,10 +29,10 @@ public class CaesarCipher {
         if (cipher1.length() != cipher2.length())
             return false;
 
-        for (int i = 0; i < cipher1.length()-1; i++) {
+        for (int i = 0; i < cipher1.length(); i++) {
             int a = cipher1.charAt(i);
             int b = cipher2.charAt(i);
-            //System.out.println(a + " " + b + " difference is : " + (a-b));
+            System.out.println(a + " " + b + " difference is : " + (a-b));
             if (a > b)
                 b += 26;
 
@@ -67,23 +67,27 @@ public class CaesarCipher {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string to encode");
+//        System.out.println("Enter a string to encode");
+//
+//        String input = sc.nextLine();
+//        String encoded = CaesarCipher.encode(input, 25);
+//        System.out.println(encoded);
+//
+//        String decoded = CaesarCipher.decode(encoded, 25);
+//        System.out.println(decoded);
 
-        String input = sc.nextLine();
-        String encoded = CaesarCipher.encode(input, 25);
-        System.out.println(encoded);
-
-        String decoded = CaesarCipher.decode(encoded, 25);
-        System.out.println(decoded);
-
-        System.out.println("Enter two strings to compare");
+        System.out.println("Enter the first string to compare");
         String input1 = sc.nextLine();
         String cipher1 = encode(input1, 4);
+        System.out.println(cipher1);
+
+        System.out.println("\nEnter the second string to compare");
         String input2 = sc.nextLine();
         String cipher2 = encode(input2, 22);
+        System.out.println(cipher2);
 
-        System.out.println(codeBreaker(cipher1,cipher2));
-        System.out.println(codeBreaker2(cipher1,cipher2));
+        System.out.println("\nThe result provided by codeBreaker is : " + codeBreaker(cipher1,cipher2));
+        System.out.println("The result provided by codeBreaker2 is : " + codeBreaker2(cipher1,cipher2));
 
     }
 }
