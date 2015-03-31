4964b0011491b1c83d790098c3c1509e42c8f243
Luke Lee <samsung3star@gmail.com>
2015-03-31 19:31:15 -0400

    in the middle

---
+++ AC_20150331/src/nyc/c4q/lukesterlee/WordTracker.java
@@ -0,0 +1,51 @@
+package nyc.c4q.lukesterlee;
+
+/**
+ * Created by Luke Lee on 3/31/15.
+ */
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WordTracker {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        String word;
+        boolean addMode = true;
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
+        System.out.println("Please give me some words:");
+
+        while (true) {
+
+            word = input.nextLine();
+            if (word.equals("stop"))
+                break;
+            else if (word.equals("rm")) {
+                addMode = false;
+                continue;
+            } else if (word.equals("add")) {
+                addMode = true;
+                continue;
+            } else if (word.equals("clr")) {
+                words.clear();
+                continue;
+            }
+
+
+            if (addMode)
+                words.put(word, 1);
+            else
+                words.remove(word);
+        }
+
+        System.out.println("Thanks! You have given me " + words.size() + " word(s)!");
+
+
+
+
+    }
+
+}
