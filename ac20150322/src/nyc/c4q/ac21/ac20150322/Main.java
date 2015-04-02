d6a979e7ab6987baa04de6106c27b05e4631aa70
Luke Lee <samsung3star@gmail.com>
2015-04-02 11:17:53 -0400

    URL

---
+++ ac20150322/src/nyc/c4q/ac21/ac20150322/Main.java
@@ -0,0 +1,43 @@
+package nyc.c4q.ac21.ac20150322;
+
+import java.net.URL;
+
+/**
+ * Created by Luke Lee on 4/1/15.
+ */
+public class Main {
+
+    public static URL makeHttpUrl(String host, int port, String path) {
+
+        String address = "http://" + host;
+
+        if (port != 80)
+            address += ":" + port;
+
+        address += path;
+
+        return HTTP.stringToURL(address);
+    }
+
+    public static URL makeHttpUrl(String host, String path) {
+        return makeHttpUrl(host, 80, path);
+    }
+
+    public static void printUrl(URL url) {
+        System.out.println(url.getHost() + url.getPath());
+        System.out.println();
+
+        System.out.println(url.getProtocol());
+        System.out.println(url.getHost());
+        System.out.println(url.getPort());
+        System.out.println(url.getPath());
+    }
+
+    public static void main(String[] args) {
+        URL url = makeHttpUrl("github.com", "/accesscode-2-1/unit-0");
+        System.out.println(url);
+        System.out.println();
+
+        printUrl(url);
+    }
+}
