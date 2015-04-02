ea52e5a0278fc7f661c50c62b25c737d134dfdbc
Luke Desktop <lukesterlee@gmail.com>
2015-04-02 17:03:34 -0400

    update

--- Review/src/nyc/c4q/lukesterlee/PassByValue.java
+++ Review/src/nyc/c4q/lukesterlee/PassByValue.java
@@ -16,7 +16,28 @@ public class PassByValue {
         y = temp;
     }
 
-    public static void main(String[] args) {
+    public static void doSomething(int x, int y) {
+        x = 100;
+        y = 200;
+    }
 
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        doSomething(a, b);
+        System.out.println(a + " " + b);
+
+        int sum = 0;
+        for (int i = 1; i <= 10; i++)
+            sum += i;
+        System.out.println("Sum from 1 to 10 is " + sum);
+        sum = 0;
+        for (int i = 20; i <= 30; i++)
+            sum += i;
+        System.out.println("Sum from 20 to 30 is " + sum);
+        sum = 0;
+        for (int i = 35; i <= 45; i++)
+            sum += i;
+        System.out.println("Sum from 35 to 45 is " + sum);
     }
 }
