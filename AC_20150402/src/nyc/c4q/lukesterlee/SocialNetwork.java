da450cae476c49576859b2afb9ce81030ff643db
Luke Lee <samsung3star@gmail.com>
2015-04-02 20:57:15 -0400

    today's exercise

--- AC_20150402/src/nyc/c4q/lukesterlee/SocialNetwork.java
+++ AC_20150402/src/nyc/c4q/lukesterlee/SocialNetwork.java
@@ -15,46 +15,103 @@ public class SocialNetwork {
         mine.add("Eve");
 
         // You should be able to run all of these in a row:
-        //System.out.println( allFriends(yours, mine) );
-        //System.out.println( mutualFriends(yours, mine) );
-        //System.out.println( yourFriends(yours, mine) );
-        //System.out.println( myFriends(yours, mine) );
-        //System.out.println( exclusiveFriends(yours, mine) );
-        //System.out.println( yourFriendsAreMine(yours, mine) );
-        //System.out.println( youHaveFriends(yours, mine) );
-        //System.out.println( matchmaker(yours, mine) );
+        System.out.println( allFriends(yours, mine) );
+        System.out.println( mutualFriends(yours, mine) );
+        System.out.println( justYourFriends(yours, mine) );
+        System.out.println( justMyFriends(yours, mine) );
+        System.out.println( exclusiveFriends(yours, mine) );
+        System.out.println( yourFriendsAreMine(yours, mine) );
+        System.out.println( youHaveFriends(yours) );
+        System.out.println( matchmaker(yours, mine) );
     }
 
-    /*public static HashSet<String> allFriends(HashSet<String> yours, HashSet<String> mine) {
-        // Yelena, Jamal, Eve, Sebastian, Mala
-    }*/
+    public static HashSet<String> allFriends(HashSet<String> yours, HashSet<String> mine) {
 
-    /*public static HashSet<String> mutualFriends(HashSet<String> yours, HashSet<String> mine) {
+        HashSet<String> allFriends = new HashSet<String>(mine);
+        allFriends.addAll(yours);
+        return allFriends;
+         //Yelena, Jamal, Eve, Sebastian, Mala
+    }
+
+    public static HashSet<String> mutualFriends(HashSet<String> yours, HashSet<String> mine) {
+
+        HashSet<String> mutualFriends = new HashSet<String>();
+        for (String friend : mine) {
+            if (yours.contains(friend))
+                mutualFriends.add(friend);
+        }
+        return mutualFriends;
         // Eve
-    }*/
+    }
 
-    /*public static HashSet<String> justYourFriends(HashSet<String> yours, HashSet<String> mine) {
+    public static HashSet<String> justYourFriends(HashSet<String> yours, HashSet<String> mine) {
+
+        HashSet<String> justYourFriends = new HashSet<String>(yours);
+        for (String friend : mine) {
+            if (justYourFriends.contains(friend))
+                justYourFriends.remove(friend);
+        }
+        return justYourFriends;
         // Yelena, Jamal
-    }*/
+    }
 
-    /*public static HashSet<String> justMyFriends(HashSet<String> yours, HashSet<String> mine) {
+    public static HashSet<String> justMyFriends(HashSet<String> yours, HashSet<String> mine) {
+
+        HashSet<String> justMyFriends = new HashSet<String>(mine);
+        for (String friend : yours) {
+            if (justMyFriends.contains(friend))
+                justMyFriends.remove(friend);
+        }
+        return justMyFriends;
         // Sebastian, Mala
-    }*/
+    }
 
-    /*public static HashSet<String> exclusiveFriends(HashSet<String> yours, HashSet<String> mine) {
+    public static HashSet<String> exclusiveFriends(HashSet<String> yours, HashSet<String> mine) {
+
+        HashSet<String> exclusiveFriends = new HashSet<String>(mine);
+        exclusiveFriends.addAll(yours);
+        exclusiveFriends.removeAll(mutualFriends(yours, mine));
+
+        return exclusiveFriends;
         // Yelena, Jamal, Sebastian, Mala
-    }*/
+    }
 
-    /*public static boolean yourFriendsAreMine(HashSet<String> yours, HashSet<String> mine) {
+    public static boolean yourFriendsAreMine(HashSet<String> yours, HashSet<String> mine) {
+
+        for (String friend : yours) {
+            if (!mine.contains(friend))
+                return false;
+        }
+        return true;
         // true if all your friends are also my friends
-    }*/
 
-    /*public static boolean youHaveFriends(HashSet<String> yours) {
+    }
+
+    public static boolean youHaveFriends(HashSet<String> yours) {
+
+        return !yours.isEmpty();
         // true if you have any friends
-    }*/
+    }
 
-    /*public static HashSet<HashSet<String>> matchmaker(HashSet<String> yours, HashSet<String> mine) {
+    public static HashSet<HashSet<String>> matchmaker(HashSet<String> yours, HashSet<String> mine) {
+
         // should return these sets:
+        HashSet<HashSet<String>> matchMaker = new HashSet<HashSet<String>>();
+
+        for (String friend : mine) {
+            for (String match : yours) {
+                HashSet<String> couple = new HashSet<String>();
+                if (!match.equals(friend)) {
+                    couple.add(friend);
+                    couple.add(match);
+                    matchMaker.add(couple);
+                }
+
+            }
+        }
+
+        return matchMaker;
+
 
         // Sebastian, Jamal
         // Jamal, Mala
@@ -66,5 +123,5 @@ public class SocialNetwork {
 
         // Sebastian, Eve
         // Mala, Eve
-    }*/
+    }
 }
